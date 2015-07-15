be67b52bdfee412328c223a9a58b93b8986affc2
Erica Kim <ericakim@Ericas-MacBook-Pro.local>
2015-07-15 12:38:57 -0400

    made mac img and aboutus section respoinsive

--- contents/js/main.js
+++ contents/js/main.js
@@ -33,45 +33,6 @@ $(document).ready(function () { // Document ready
     /*	01. NAVBAR STICKY + SELECTED
 /*-----------------------------------------------------------------------------------*/
 
-
-
-    var cbpAnimatedHeader = (function () {
-
-        var docElem = document.documentElement,
-            header = document.querySelector('.cbp-af-header'),
-            didScroll = false,
-            changeHeaderOn = 100;
-
-        function init() {
-            window.addEventListener('scroll', function (event) {
-                if (!didScroll) {
-                    didScroll = true;
-                    setTimeout(scrollPage, 0);
-                }
-            }, false);
-        }
-
-        function scrollPage() {
-            var sy = scrollY();
-            if (sy >= changeHeaderOn) {
-                classie.add(header, 'cbp-af-header-shrink');
-            } else {
-                classie.remove(header, 'cbp-af-header-shrink');
-            }
-            didScroll = false;
-        }
-
-        function scrollY() {
-            return window.pageYOffset || docElem.scrollTop;
-        }
-
-        init();
-
-    })();
-
-
-
-
 /*-----------------------------------------------------------------------------------*/
     /*	02. FLEXSLIDER - TESTIMONIAL
 /*-----------------------------------------------------------------------------------*/
@@ -96,52 +57,6 @@ $(document).ready(function () { // Document ready
         sync: "#slider1",
         slideshowSpeed: 3000,
         slideToStart: 0,
-    });
-
-
-
-
-/*-----------------------------------------------------------------------------------*/
-    /*	03. Share Button hover
-/*-----------------------------------------------------------------------------------*/
-
-
-    $(".cbp-af-header .right").hover(
-        function (e) {
-            $('.cbp-af-header .social-ico').fadeIn();
-        }, // over
-        function (e) {
-            $('.cbp-af-header .social-ico').fadeOut();
-        } // out
-    );
-
-
-
-/*-----------------------------------------------------------------------------------*/
-    /*	04. Drag-it img fade Out on Click
-/*-----------------------------------------------------------------------------------*/
-
-//
-//    $("#dragit").click(function () {
-//        $("#dragit").fadeOut("slow");
-//    });
-
-
-
-/*-----------------------------------------------------------------------------------*/
-    /*	05. Initialise the Carousel
-/*-----------------------------------------------------------------------------------*/
-
-
-    $("#template-2").owlCarousel({
-
-        navigation: false, // Show next and prev buttons
-        slideSpeed: 300,
-        lazyLoad: true,
-        paginationSpeed: 400,
-        singleItem: true,
-        autoHeight: false,
-        transitionStyle: "fade",
     });
 
 
@@ -194,103 +109,6 @@ $(document).ready(function () { // Document ready
         return re.test(email);
     }
 
-
-/*-----------------------------------------------------------------------------------*/
-    /*	07. MEDIACHECK
-/*-----------------------------------------------------------------------------------*/
-
-
-    mediaCheck({
-        media: '(max-width: 768px)',
-        entry: function () {
-
-            $('.make-it-appear-top').waypoint(function (direction) {
-                $(this).css('opacity', '1');
-            }, {
-                offset: '200%'
-            });
-
-            $('.make-it-appear-left').waypoint(function (direction) {
-                $(this).css('opacity', '1');
-            }, {
-                offset: '200%'
-            });
-
-            $('.make-it-appear-right').waypoint(function (direction) {
-                $(this).css('opacity', '1');
-            }, {
-                offset: '200%'
-            });
-
-            $('.make-it-appear-bottom').waypoint(function (direction) {
-                $(this).css('opacity', '1');
-            }, {
-                offset: '200%'
-            });
-
-
-        },
-        exit: function () {
-
-/*-----------------------------------------------------------------------------------*/
-            /*	08. ANNIMATIONS MAKE IT APPEAR
-/*-----------------------------------------------------------------------------------*/
-
-
-            $('.make-it-appear-top').waypoint(function (direction) {
-                $(this).addClass('animated fadeInDown');
-            }, {
-                offset: '80%'
-            });
-
-            $('.make-it-appear-left').waypoint(function (direction) {
-                $(this).addClass('animated fadeInLeft');
-            }, {
-                offset: '80%'
-            });
-
-            $('.make-it-appear-right').waypoint(function (direction) {
-                $(this).addClass('animated fadeInRight');
-            }, {
-                offset: '80%'
-            });
-
-            $('.make-it-appear-bottom').waypoint(function (direction) {
-                $(this).addClass('animated fadeInUp');
-            }, {
-                offset: '80%'
-            });
-
-            $('.bounce').waypoint(function (direction) {
-                $(this).addClass('animated bounce');
-            }, {
-                offset: '70%'
-            });
-
-            $('.pulse').waypoint(function (direction) {
-                $(this).addClass('animated pulse');
-            }, {
-                offset: '50%'
-            });
-
-/*-----------------------------------------------------------------------------------*/
-            /*	09. VIDEO BACKGROUND READY
-/*-----------------------------------------------------------------------------------*/
-
-
-            var videobackground = new $.backgroundVideo($('.page1'), {
-                "align": "centerXY",
-                "width": 1280,
-                "height": 720,
-                "path": "http://www.baribal.be/exho/video/", /* Folder wich contain the mp4, ogg and webm video */
-                "filename": "desktop",
-                "types": ["mp4", "ogg", "webm"]
-            });
-
-        }
-
-
-    }); /* END OF THE MEDIACHECK */
 
 
 }); /* END OF Document Ready */
