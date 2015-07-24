1021be0781043e7d17da555c7b6695fd8e648a9a
Erica Kim <ericakim@Ericas-MacBook-Pro.local>
2015-07-24 15:10:17 -0400

    extra section and platform logos; added

--- contents/js/main.js
+++ contents/js/main.js
@@ -35,6 +35,25 @@ $(document).ready(function () { // Document ready
     $(function() {
     $(".rslides").responsiveSlides();
   });
+    
+    
+    
+    /* fix vertical when not overflow
+call fullscreenFix() if .fullscreen content changes */
+function fullscreenFix(){
+    var h = $('body').height();
+    // set .fullscreen height
+    $(".backgroundImage").each(function(i){
+        if($(this).innerHeight() <= h){
+            $(this).closest(".fullscreen").addClass("not-overflow");
+        }
+    });
+}
+$(window).resize(fullscreenFix);
+fullscreenFix();
+    
+    
+    
 /*-----------------------------------------------------------------------------------*/
     /*	02. FLEXSLIDER - TESTIMONIAL
 /*-----------------------------------------------------------------------------------*/
